8e22f178ece5f02dff87a891570d2ec48cbc9eb0
netsenior <netsenior@gmail.com>
2017-05-04 23:51:50 -0300

    broadcasting

--- public/js/index.js
+++ public/js/index.js
@@ -1,31 +1,14 @@
 var socket = io();
+
 socket.on('connect',function(){
-	console.log('connected to server');
-	
-	//
-	// cliente envia mensagem ao servidor para ser distribuida no chat
-	//
-	socket.emit('createMessage',{
-		from:'cicrano',
-		text:'cole rei'
-	});
-	//
-	//
+	console.log('connected to server');	
 	
 });
+
 socket.on('disconnect',function(){
 	console.log('disconnected from server');
 });
 
-//
-// cliente recebe mensagem do servidor
-//
-socket.on('newMessage',function(msg){
-	console.log('new message', msg);
+socket.on('newMessage',function(message){
+	console.log('new message', message);
 });
-//
-//
-
-function envia(){
-	
-};
--- server/server.js
+++ server/server.js
@@ -14,43 +14,14 @@ app.use(express.static(publicPath));
 io.on('connection', (socket)=>{
 	console.log('new user connected');
 	
-	
-	
-	//
-	// servidor envia mensagem aos clientes
-	//
-	socket.emit('newMessage',{
-		from: 'fulano',
-		text: 'oii, tudo bem?',
-		createdAt: '2017/06/21 20:30',
+	socket.on('createMessage',(message)=>{
+		console.log('createMessage', message);
+		io.emit('newMessage', {
+			from: message.from,
+			text: message.text,
+			createdAt: new Date().getTime()
+		});
 	});
-	//
-	
-	//
-	// servidor recebe mensagem de um cliente a ser distribuido
-	//
-	socket.on('createMessage',(msg)=>{
-		console.log('createMessage', msg);
-		//
-		// servidor envia mensagem aos clientes
-		//
-		/*socket.emit('newMessage',{
-			from: msg->from,
-			text: msg->text,
-			createdAt: 12121,
-		});*/
-		//
-		//
-		console.log('createMessage',msg);
-		
-	});
-	//
-	//
-	
-	
-	
-	
-	
 	
 	socket.on('disconnect',()=>{
 		console.log('user was disconnected from server')
@@ -59,4 +30,4 @@ io.on('connection', (socket)=>{
 
 server.listen(port, ()=>{
 	console.log('server is up on '+ port)
-})
+});
